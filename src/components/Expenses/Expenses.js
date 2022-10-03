8a296309863f2073ca7144b7a98b9951788e05af
Muhammad Ahsan Farooq <ahsan.farooq@rolustech.net>
2022-10-03 15:27:35 +0500

    added charts

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
-import ExpensesList from './ExpensesList';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
   /**
-   * this function filters the list that we are receiving through the props and it 
-   * filters the list accouring to the year that we have selected in the dropdown 
+   * this function filters the list that we are receiving through the props and it
+   * filters the list accouring to the year that we have selected in the dropdown
    * that year is stored in the filteredYear state
    */
-  const filteredExpenses=props.items.filter((expense)=>{
-    return expense.date.getFullYear().toString()===filteredYear;
-  })
-
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   /**
-   * we begin with storing that we have no expense for the year that we have selected form the dropdown 
+   * we begin with storing that we have no expense for the year that we have selected form the dropdown
    * we have stored jsx in the variable
    */
 
-
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesChart expense={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
